d5fed1986f8f783f3db9176b898fcc2e340ce666
David Wang <rexmagnusdavid@gmail.com>
2025-09-20 23:33:52 -0400

    sign up

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { FormEvent } from "react";
 
 export default function Page({ params }: { params: { slug: string } }) {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(event);
+
+    const formData = new FormData(event.currentTarget);
+
+    // 3. You can now access the data
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    console.log('Username from FormData:', username);
+    console.log('Password from FormData:', password);
+
+    const response = await axios.post("http://127.0.0.1:8000/users/login", {username: username, password: password});
+    console.log(response);
+
+    const data: {access_token: string} = response.data;
+
+    document.cookie = "jwt=" + data.access_token;
+  }
+ 
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="flex min-h-screen w-full flex-col bg-gradient-to-b from-violet-600 to-indigo-600">
@@ -20,28 +46,24 @@ export default function Page({ params }: { params: { slug: string } }) {
                   <Button variant="link">Sign Up</Button>
                 </CardHeader>
                 <CardContent>
-                  <form>
+                  <form onSubmit={onSubmit}>
                     <div className="flex flex-col gap-6">
                       <div className="grid gap-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" placeholder="m@example.com" required />
+                        <Label htmlFor="text">Username</Label>
+                        <input name="username" type="text" placeholder="hello@example.com" required />
                       </div>
                       <div className="grid gap-2">
-                        <div className="flex items-center">
-                          <Label htmlFor="password">Password</Label>
-                          <a href="#" className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
-                            Forgot your password?
-                          </a>
-                        </div>
-                        <Input id="password" type="password" required />
+                        <Label htmlFor="password">Password</Label>
+                        <input name="password" type="password" required />
                       </div>
+                      <button type="submit" className="w-full">
+                        Login
+                      </button>
                     </div>
                   </form>
                 </CardContent>
                 <CardFooter className="flex-col gap-2">
-                  <Button type="submit" className="w-full">
-                    Login
-                  </Button>
+
                 </CardFooter>
               </Card>
             </div>
